824814016514d962494802c111e48efefb6c2e59
PhanHoang12 <phanhln.23it@vku.udn.vn>
2024-11-29 23:54:49 +0700

    update booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
-
+    protected $table = 'bookings';
     protected $fillable = [
         'user_id',
         'service_id',
